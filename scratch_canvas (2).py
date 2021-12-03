e5544294baa7b31189916539564859543aeacfb1
devenkhichar <95070492+devenkhichar@users.noreply.github.com>
2021-12-03 20:00:37 +0530

    Add files via upload

---
+++ scratch_canvas (2).py
@@ -0,0 +1,16 @@
+import turtle
+for i in range(2, 100):
+    my7u = turtle.speed(0)
+    turtle.pencolor('blue')
+    turtle.forward(500)
+    turtle.right(145)
+for i in range(2, 100):
+    turtle.pencolor('red')
+    my7u = turtle.speed(0)
+    turtle.forward(500)
+    turtle.right(146)
+for i in range(2, 100):
+    turtle.pencolor('yellow')
+    my7u = turtle.speed(0)
+    turtle.forward(500)
+    turtle.right(147)
